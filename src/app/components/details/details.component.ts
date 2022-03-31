3ee16cc2ef1dfbdd5d7ebf3928aff8fb38b639df
soumallyadev07 <devsoumallya@gmail.com>
2022-03-31 12:22:10 +0530

    Dynamic Details Page

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   gameRating = 0;
-  constructor() { }
+  gameId!: string;
+  game!: Game;
+  routeSub!: Subscription;
+  gameSub!: Subscription;
+
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) { }
 
   ngOnInit(): void {
+    this.routeSub = this.ActivatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
+  }
+
+  getGameDetails(id: string): void{
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((gameResp: Game) => {
+        this.game = gameResp;
+
+        setTimeout(() => {
+          this.gameRating = this.game.metacritic;
+        }, 1000);
+      });
   }
 
   getColor(value: number): string{
@@ -21,6 +49,15 @@ export class DetailsComponent implements OnInit {
       return '#f7aa38';
     } else{
       return '#ef4655';
+    }
+  }
+
+  ngOnDestroy(): void{
+    if(this.gameSub){
+      this.gameSub.unsubscribe();
+    }
+    if(this.routeSub){
+      this.routeSub.unsubscribe();
     }
   }
 
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, map, Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
 import { APIResponse, Game } from '../models';
 
@@ -26,5 +26,28 @@ export class HttpService {
     });
   }
 
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailerRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailerRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+  }
 
 }
